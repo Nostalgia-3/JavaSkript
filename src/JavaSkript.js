f15f0b4c75152cc2e50a9d608ca49f70ee0a51b0
Santiago Mendez <legotronforce@gmail.com>
2021-01-24 23:48:23 -0800

    First Commit

--- src/JavaSkript.js
+++ src/JavaSkript.js
@@ -2,11 +2,35 @@ var fs = require("fs");
 var data = "";
 var currentTabValue = "";
 
-// Commands
-
+// EFFECTS
 function broadcast(text) {
     data = data + ("\n" + currentTabValue + "broadcast \"" + text + "\"");
 }
+
+function branch(num,func) {
+    for(var x = 0; x<num;x++) {
+        func();
+    }
+}
+
+function ban(player,reason=null,time=null) {
+    let banMessage = `ban ${player} `;
+
+    if (reason !=null) {
+        banMessage = banMessage + `due to "${reason}"`;
+    }
+    if (time != null) {
+        banMessage = banMessage + ` for ${time}`;
+    }
+    
+    console.log(`ban-effect found: banMessage:${banMessage}, currentTabValue:${currentTabValue.length}`);
+
+    data = data + "\n" + currentTabValue + banMessage;
+};
+
+function unban(player) {
+    data = data + "\n" + currentTabValue + `unban ${player}`
+};
 
 // EVENTS
 function onLoad(func) {
@@ -31,4 +55,4 @@ function finish() {
     console.log("Finished update of \"output.sk\". DO NOT MAKE MORE LINES OF CODE AFTER THIS AS IT DOES NOT SAVE");
 };
 
-module.exports = {broadcast,onLoad,onPlayerJoin,finish};
+module.exports = {broadcast,onLoad,onPlayerJoin,branch,ban,unban,finish};

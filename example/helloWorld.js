7c0a92fdcdb07de97d610ed9d02cb3710916a911
Santiago Mendez <legotronforce@gmail.com>
2021-01-25 12:32:25 -0800

    Made helloWorld better

--- example/helloWorld.js
+++ example/helloWorld.js
@@ -1,10 +1,8 @@
-const {broadcast,onLoad,onPlayerJoin,branch,command,actionBar,registerRecipe,ban,unban,finish} = require("../src/JavaSkript");
+const {broadcast,onLoad,finish} = require("../src/JavaSkript");
 
 
 onLoad(() => {
     broadcast("Hello, world!");
-    ban("notch","being the creator of minecraft","10 days");
-    unban("notch");
 });
 
 finish();
--- test/main.js
+++ test/main.js
@@ -3,8 +3,6 @@ const {onLoad, broadcast, ban, unban, finish} = require("../src/JavaSkript");
 
 onLoad(() => {
     broadcast("Hello, world!");
-    ban("notch","being the creator of minecraft","10 days");
-    unban("notch");
 });
 
 finish();
